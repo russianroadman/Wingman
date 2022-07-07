ae82ac2521232f741a78c7b70ff09a348f451697
russianroadman <realgar2@gmail.com>
2022-07-08 00:32:55 +0800

    Initial

---
+++ src/test/java/ru/russianroadman/wingman/WingmanApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.russianroadman.wingman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WingmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
